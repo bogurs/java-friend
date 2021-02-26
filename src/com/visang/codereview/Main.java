a093a614a37b31e52d74ae913cba6c819d06969c
leebc <leebc@visang.com>
2021-02-27 00:22:45 +0900

    Test Commit2

--- src/com/visang/codereview/Main.java
+++ src/com/visang/codereview/Main.java
@@ -35,11 +35,9 @@ public class Main {
      */
     private static int getCountTwoLengthCorrectly(String[] fruits) {
         int result = 0;
-        //TODO fruits 배열에서 2글자로 된 단어의 총 개수를 result 변수에 저장하는 함수 내용을 작성해주세요. test
+        //TODO fruits 배열에서 2글자로 된 단어의 총 개수를 result 변수에 저장하는 함수 내용을 작성해주세요.
         int a = 0;
         int b = 0;
-
-
 
 
 
@@ -55,7 +53,7 @@ public class Main {
         int result = 0;
 
         //TODO fruits 배열에서 "바나나" 의 총 개수를 result 변수에 저장하는 함수 내용을 작성해주세요.
-        int aaa = 333;
+        int i = 55;
 
         return result;
     }
@@ -69,7 +67,6 @@ public class Main {
         String[] result = new String[fruits.length];
 
         //TODO fruits 배열에서 중복이 되지 않는 항목들만 result 변수에 저장하는 함수 내용을 작성해주세요.
-        int a = 000000;
 
         return result;
     }
