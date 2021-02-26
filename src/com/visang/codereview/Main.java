dcfb0e256366832da4958076c4b7625a47e0373d
leebc <leebc@visang.com>
2021-02-26 20:04:30 +0900

    pull request

--- src/com/visang/codereview/Main.java
+++ src/com/visang/codereview/Main.java
@@ -35,7 +35,7 @@ public class Main {
      */
     private static int getCountTwoLengthCorrectly(String[] fruits) {
         int result = 0;
-        //TODO fruits 배열에서 2글자로 된 단어의 총 개수를 result 변수에 저장하는 함수 내용을 작성해주세요.
+        //TODO fruits 배열에서 2글자로 된 단어의 총 개수를 result 변수에 저장하는 함수 내용을 작성해주세요. test
         int a = 0;
         int b = 0;
 
