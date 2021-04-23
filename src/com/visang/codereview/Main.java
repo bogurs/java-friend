adf11a75d93f5d7040e6cf3a3efff84d106b3b0c
vs-leebc <leebc@visang.com>
2021-04-23 10:57:30 +0900

    또다시 수정6122233 또다시 수정61222 또다시 수정61

--- src/com/visang/codereview/Main.java
+++ src/com/visang/codereview/Main.java
@@ -1,31 +1,46 @@
 package com.visang.codereview;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 문제 제시
  * 1.	주어진 배열을 가지고 아래 문제에 대한 소스코드를 작성해주세요.
  *      String[] fruits = {"애플", "포도", "감", "바나나", "파인애플", "귤", "바나나", "키위", "바나나"};
  * A.	한글 2글자로 된 단어의 개수를 알아내는 함수
  * B.	"바나나" 가 몇 번 나왔는지 알아내는 함수
- * C.	중복을 제거하여 문자열 배열을 반환하는 함수
+ * C.	중복을 제거하여 문자열 배열을 반환하는 함수1122dd
  * D.	"애플" 이 포함된 단어의 개수를 알아내는 함수
- */
+ */1
+
+
 public class Main {
+
+    static class Member {
+        String name;
+        int age;
+
+        public Member(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
 
     public static void main(String[] args) {
 	    //TODO: 아래 제시된 함수가 원하는 결과를 콘솔화면에 출력하도록 함수 내용을 완성해주세요.
-        String[] fruits = {"애플", "포도", "감", "바나나", "파인애플", "귤", "바나나", "키위", "바나나"};
 
-        int result1 = getCountTwoLengthCorrectly(fruits);
-        int result2 = getCountAllBananas(fruits);
-        String[] result3 = getStringArrayWithoutOverlap(fruits);
-        int result4 = getCountContainApple(fruits);
+        List<Member> list = new ArrayList<>();
+        List<Member> list1 = new ArrayList<>();
 
-        System.out.println(result1); // 원하는 결과: 3
-        System.out.println(result2); // 원하는 결과: 3
-        for (String fruit : result3) {
-            System.out.print(fruit + " "); // 원하는 결과: 애플 포도 감 바나나 파인애플 귤 키위
-        }
-        System.out.println(result4); // 원하는 결과: 2
+        list.add(new Member("차다희", 11));
+        list1.add(new Member("차다", 11));
+
+        System.out.println(list.hashCode());
+        System.out.println(list1.hashCode());
+        System.out.println(list.get(0).hashCode());
+        System.out.println(list1.get(0).hashCode());
+        System.out.println(list.get(0).name.hashCode());
+        System.out.println(list1.get(0).name.hashCode());
     }
 
     /**
